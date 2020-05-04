2f48a3b068e8a4d41a64e8287a74f4e87fcd3d0b
Ryan Duong <ryanduong0426@gmail.com>
2020-05-03 20:25:12 -0700

    commit1

--- AutomateExpenseReport/AutomateExpenseReport.py
+++ AutomateExpenseReport/AutomateExpenseReport.py
@@ -1,3 +1,10 @@
 import csv
 
+"""Script to Automate the expense report proccess as much as possible
+"""
 
+with open('expensereport.csv') as csvFile:
+    csvReader = csv.reader(csvFile)
+
+    for i in csvReader:
+        
