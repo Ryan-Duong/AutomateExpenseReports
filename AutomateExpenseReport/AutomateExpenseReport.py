7fde8a8e096836c714325ffe777d87fc8e9a461d
Ryan Duong <ryanduong0426@gmail.com>
2020-05-11 22:00:32 -0700

    commit

--- AutomateExpenseReport/AutomateExpenseReport.py
+++ AutomateExpenseReport/AutomateExpenseReport.py
@@ -7,9 +7,9 @@ as well as different codes depending on amounts based off merchants
 with open(r'C:\Users\thech\Downloads\expensereport.csv', mode = 'r') as csvFile:
     csvReader = csv.reader(csvFile)
 
-    with open(r'C:\Users\thech\Downloads\expensereport2.csv', mode = 'w') as csvFile2:
+    with open(r'C:\Users\thech\Downloads\expensereport2.csv', mode = 'w', newline = '') as csvFile2:
         csvWriter = csv.writer(csvFile2, delimiter = ',', quotechar = '"')
        
         for line in csvReader:
-            line[1].replace('-', '')
+            line[1] = line[1].replace('-', '')
             csvWriter.writerow(line)
